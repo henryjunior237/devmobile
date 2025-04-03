24d0406c440697febff75519d627ac8df838a810
henryjunior237 <162426232+henryjunior237@users.noreply.github.com>
2025-04-03 17:55:32 +0100

    push1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonTabButton, IonIcon, IonLabel,IonList, IonItem, IonCard, IonCardTitle, IonCardHeader, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCardContent, IonCardSubtitle, IonCardHeader, IonCardTitle, IonCard, IonItem, IonLabel, IonIcon, IonTabButton, IonButton, IonHeader, IonToolbar, IonTitle, IonContent,IonList],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private alertController: AlertController ) {
+  }
 }
